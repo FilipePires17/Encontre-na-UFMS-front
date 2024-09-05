1f0dddbc96cc71dfebea900e7e385362f065fe6e
filipe <filipepiresec@gmail.com>
2024-09-04 22:53:47 -0400

    primeiro commit

---
+++ android/app/src/main/kotlin/com/example/encontre_na_ufms/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.encontre_na_ufms
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:encontre_na_ufms/src/app.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
