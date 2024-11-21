20fe0fd25c075a18a0a6c45358d3124911e0d8a2
filipe <filipepiresec@gmail.com>
2024-11-21 08:26:55 -0400

    texto inicial da tela sobre

--- lib/src/features/about/presentation/about_screen.dart
+++ lib/src/features/about/presentation/about_screen.dart
@@ -17,13 +17,9 @@ class AboutScreen extends StatelessWidget {
         padding:
             EdgeInsets.symmetric(horizontal: Sizes.p16, vertical: Sizes.p8),
         child: Text(
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. '
-          'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '
-          'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris '
-          'nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in '
-          'reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla '
-          'pariatur. Excepteur sint occaecat cupidatat non proident, sunt in '
-          'culpa qui officia deserunt mollit anim id est laborum.',
+          'Desenvolvido por Filipe dos Santos Pires e Wilson Eduardo Fantucci'
+          ' Diniz de Almeida como trabalho de conclusão do curso de Engenharia'
+          ' de Computação da Universidade Federal de Mato Grosso do Sul.',
           style: TextStyle(fontSize: Sizes.p16),
         ),
       ),
